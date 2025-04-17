825b525804be0340a54e40d78ba830b2e7fada8f
Kai_Hogan <kaidavishogan@gmail.com>
2025-04-17 11:10:37 -0700

    Initial commit

---
+++ week_2/coding_problems.py
@@ -0,0 +1,38 @@
+"""Problem 1: Write a function that takes in three numbers and returns the
+greatest of the three.
+"""
+def greatest(a:int, b:int, c:int):
+    return
+
+
+"""Problem 2: Write a function that takes in two numbers a and b
+and returns the sum of all the numbers from a to b (not including b).
+Ex: sum_of_range(3,6)->12
+"""
+def sum_of_range(a:int, b:int):
+    return
+
+
+"""Problem 3: Write a function that takes one number a and divides it by another number 
+b and returns the quotient rounded up 
+"""
+def ceiling_division(a:int, b:int):
+    return
+
+
+"""Problem 4: Write a function that takes in three different integers and 
+returns the integer with the intermediate value IE the value in the middle.
+Ex: middle(4,12,7)->7
+"""
+def middle(a:int, b:int, c:int):
+    return
+
+
+"""Problem 5: Write a function that takes in an integer and returns the
+greatest common denominator.
+Ex: GCD(15)->5
+"""
+def GCD(a:int):
+    return
+
+
